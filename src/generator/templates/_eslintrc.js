eebe399697a50c5332448f89113dd26f70d9d2a3
sniperyan <304787171@qq.com>
2018-05-30 19:30:18 +0800

    add

--- src/generator/templates/_eslintrc.js
+++ src/generator/templates/_eslintrc.js
@@ -1,5 +1,14 @@
 module.exports = {
-    extends: [   
+    extends: [
         'fbjs'
-    ] 
+    ],
+    "rules": {
+        'jsx-a11y/no-static-element-interactions': [
+            'error',
+            {
+              handlers: [
+              ]
+            }
+          ]
+    }
 }
