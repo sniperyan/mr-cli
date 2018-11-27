27e3aae94d4b75f6dbb885031e0d7f3fc3c58fa7
sniperyan <sniperyan@sniperyandeMacBook-Air.local>
2018-11-27 16:01:16 +0800

    v1.0.2

--- src/generator/templates/src/scripts/containers/home/index.js
+++ src/generator/templates/src/scripts/containers/home/index.js
@@ -1,12 +1,16 @@
+/*eslint no-console: 'off'*/
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as homeAction from '../../actions/home';
 import logo from '../../../assets/logo.svg';
+import acc_icon from '../../../assets/acc_icon.webp';
 import { Link } from 'react-router-dom';
 import styles1 from './app.module.css';
 import styles2 from './app.module.less';
 import './noModule.css';
+import request from '../../util/request';
+import URL_PREFIX from '../../util/urlPrefix';
 
 @connect(
   state => ({
@@ -20,7 +24,16 @@ class Home extends PureComponent {
   static propTypes = {
    
   }
-  componentWillMount() {}
+  componentDidMount() {
+    //https://cnodejs.org/api/v1/topics
+    request(`${URL_PREFIX}/api/v1/topics`, {
+      method: 'GET',
+    }).then((res) => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    });
+  }
   click=() => {
     const {actions} = this.props;
     actions.add();
@@ -38,6 +51,7 @@ class Home extends PureComponent {
         <p className="ttt">测试非模块化</p>
         <button onClick={this.click}>add</button>
         <img src={logo} alt="logo" />
+        <img src={acc_icon} alt="logo" />
         <Link to="/about">To About</Link>
         
       </div>
